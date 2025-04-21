291f872909b118449ccae765b8dcd28f83a5ce4e
shijiu <shijiu8861@gmail.com>
2025-04-21 23:02:07 +0800

    第20次上传

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4MGXFB4WJV"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4MGXFB4WJV');
+          `
+        }} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
